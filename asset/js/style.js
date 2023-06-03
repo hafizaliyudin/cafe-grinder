818b788e89493a2836ce65a4354a2913f62ca075
hafizaliyudin <hafizhaliyuddin12@gmail.com>
2023-06-03 17:13:12 +0700

    first commit

---
+++ asset/js/style.js
@@ -0,0 +1,13 @@
+const nav = document.querySelector("nav");
+
+window.addEventListener("scroll", () => {
+  console.log(window.scrollY);
+  const windowPosition = window.scrollY > 0;
+  nav.classList.toggle("scrollink-active", windowPosition);
+});
+
+const button = document.getElementById("slide");
+
+button.onclick = () => {
+  document.getElementById("scroll").scrollLeft += 700;
+};
